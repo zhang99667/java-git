290b89adf736415dd0fe55e8a06a6d12451e8191
marz <a1097732787@live.com>
2022-09-11 13:59:32 +0800

    jdbc

--- JavaWeb/com.itheima.jdbc/JDBCDemo.java
+++ JavaWeb/com.itheima.jdbc/JDBCDemo.java
@@ -1,38 +1,38 @@
 package com.itheima.jdbc;
 
-import java.beans.Statement;
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 /*
  * JDBC 快速入门
  */
-class JDBCDemo{
-public static void main(String[] args) throws Exception{
-    // 1. 注册驱动
-    Class.forName("com.mysql.jdbc.Driver");
+class JDBCDemo {
+    public static void main(String[] args) throws Exception {
+        // 1. 注册驱动
+        Class.forName("com.mysql.jdbc.Driver");
 
-    // 2. 获取链接
-    String url="jdbc:mysql://127.0.0.1:3306/db1";
-    String username="root";
-    String password="root";
-    Connection con=DriverManager.getConnection(url,username,password);
+        // 2. 获取链接
+        String url = "jdbc:mysql://127.0.0.1:3306/db1";
+        String username = "root";
+        String password = "root";
+        Connection con = DriverManager.getConnection(url, username, password);
 
-    // 3. 定义 sql
-    String sql = "update account set money = 2000 where id = 1";
+        // 3. 定义 sql
+        String sql = "update account set money = 2000 where id = 1";
 
-    // 4. 获取执行 sql 的对象 Statement
-    Statement stmt= con.createStatement();
+        // 4. 获取执行 sql 的对象 Statement
+        Statement stmt = con.createStatement();
 
-    // 5. 执行 sql
-    int count = stmt.executeUpdate(sql);
+        // 5. 执行 sql
+        int count = stmt.executeUpdate(sql);
 
-    // 6. 处理结果
-    System.out.println(count);
+        // 6. 处理结果
+        System.out.println(count);
 
-    
-    // 7. 释放资源
-    stmt.close();
-    con.close();
-    
-}}
+        // 7. 释放资源
+        stmt.close();
+        con.close();
+
+    }
+}
