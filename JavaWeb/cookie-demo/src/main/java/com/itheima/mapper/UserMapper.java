33058dedaf5820e2548d08a10cbe1afede633d0d
a1097 <a1097732787@live.com>
2022-10-11 19:44:02 +0800

    cookie

--- JavaWeb/cookie-demo/src/main/java/com/itheima/mapper/UserMapper.java
+++ JavaWeb/cookie-demo/src/main/java/com/itheima/mapper/UserMapper.java
@@ -9,17 +9,24 @@ public interface UserMapper {
 
     /**
      * 查询所有用户
+     *
      * @return List<User> 实体
      */
     List<User> selectAll();
 
-
     /**
      * 根据 username 查询用户
+     *
      * @param username 用户名
      * @return User 实体
      */
     User selectUserByUsername(@Param("username") String username);
 
+
+    /**
+     * @param username 用户名
+     * @param password 密码
+     * @return User 实体
+     */
     User login(@Param("username") String username, @Param("password") String password);
 }
